07210668310a90ed83c42c3f1232f3c27854b680
= <iremturanli@hotmail.com>
2021-08-23 13:40:28 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,12 @@
+
+from utils.func import add
+from utils.func import update,delete
+
+
+
+add("Küçük prens",1990,"adsa","das","das")
+add("Küçük Kral",2000,"addssa","dram","irem")
+add("Casper",1889,"John","Çocuk","Nurhayat")
+# update(1)
+# delete(1)
+# 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 token
